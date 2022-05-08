c4fc70f7380beb9d2f1cf4393f6cb0f8cf7e1cf7
zhaoshihao109 <55019961+zhaoshihao109@users.noreply.github.com>
2022-05-08 20:05:23 +0800

    Create day_126.cpp

---
+++ code/day_126.cpp
@@ -0,0 +1,28 @@
+/*
+给你一个长度为 n 的整数数组 nums ，其中 nums 的所有整数都在范围 [1, n] 内，且每个整数出现 一次 或 两次 。请你找出所有出现 两次 的整数，并以数组形式返回。
+
+你必须设计并实现一个时间复杂度为 O(n) 且仅使用常量额外空间的算法解决此问题。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/find-all-duplicates-in-an-array
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> result;
+        nums.insert(nums.begin(),0);
+        for(int i=1;i<nums.size();i++){
+            while(nums[i]!=nums[nums[i]]){
+                swap(nums[i],nums[nums[i]]); 
+            }
+        }
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]!=i){
+                result.push_back(nums[i]);
+            }
+        }
+        return result;
+    }
+};
