9888d72e16efe0e197dc4c0f0ab96ba7f198b1a7
zhaoshihao109 <55019961+zhaoshihao109@users.noreply.github.com>
2022-05-24 11:22:29 +0800

    Create day_136.cpp

---
+++ code/day_136.cpp
@@ -0,0 +1,33 @@
+/*
+1603. 设计停车系统
+请你给一个停车场设计一个停车系统。停车场总共有三种不同大小的车位：大，中和小，每种尺寸分别有固定数目的车位。
+
+请你实现 ParkingSystem 类：
+
+ParkingSystem(int big, int medium, int small) 初始化 ParkingSystem 类，三个参数分别对应每种停车位的数目。
+bool addCar(int carType) 检查是否有 carType 对应的停车位。 carType 有三种类型：大，中，小，分别用数字 1， 2 和 3 表示。一辆车只能停在  carType 对应尺寸的停车位中。如果没有空车位，请返回 false ，否则将该车停入车位并返回 true 。
+*/
+
+class ParkingSystem {
+public:
+    vector<int> free_number = {0,0,0};
+    ParkingSystem(int big, int medium, int small) {
+        free_number[0] = big;
+        free_number[1] = medium;
+        free_number[2] = small;
+    }
+    
+    bool addCar(int carType) {
+        if(free_number[carType-1]>0){
+            free_number[carType-1]--;
+            return 1;
+        }
+        return 0;
+    }
+};
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem* obj = new ParkingSystem(big, medium, small);
+ * bool param_1 = obj->addCar(carType);
+ */
